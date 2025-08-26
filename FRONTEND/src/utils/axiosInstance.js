cc931fbd309da062c27c69c05ded00c1f864559b
DhirajGupta141 <dhirajgupta20001@gmail.com>
2025-08-26 17:05:24 +0530

    updating

--- FRONTEND/src/utils/axiosInstance.js
+++ FRONTEND/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const axiosInstance = axios.create({
     // baseURL:"http://localhost:3000",
-     baseURL = "https://url-shortner-backend-mu.vercel.app",
+     baseURL : "https://url-shortner-backend-mu.vercel.app",
     timeout:10000, //10s
     withCredentials:true
 })
